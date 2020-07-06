18e12898a0e858ac562c2ce1520208a14b0fc339
Lukas Parescius (parescil) <parescil@coventry.ac.uk>
2020-07-06 21:07:45 +0300

    version 0.7

--- index.js
+++ index.js
@@ -21,32 +21,46 @@ var data = xlsx.utils.sheet_to_json(ws, {defval: " ",raw:false,dateNF:'yyyy-mm-d
 
 function GenerateLoginData(){
     for(u = 0; u < data.length; u++){
-
-        var randomString = "";
-        if(data[u].Username == " " || undefined){
-            for(i = 0; i < 5; i++){
-                var randomNum = Math.floor(Math.random() * 10);
-                //var randomChar = String.fromCharCode(randomNum);
-                randomString = randomString + randomNum.toString();
-            }
-            data[u].Username = data[u].First_Name + randomString;
-            randomString = "";
+        if(data[u].Is_Created == "Yes"){
+            continue;
         }
-        if(data[u].Password == " " || undefined){
-            for(i = 0; i < 5; i++){
-                var randomNum = Math.floor(Math.random() * 10);
-                //var randomChar = String.fromCharCode(randomNum);
-                randomString = randomString + randomNum.toString();
+        else{
+            var randomString = "";
+            randomString = data[u].First_Name;
+            if(randomString.charAt(0) != randomString.charAt(0).toUpperCase()){
+                randomString = randomString.replace(randomString.charAt(0) ,randomString.charAt(0).toUpperCase());
+                data[u].First_Name = randomString;
             }
-            for(i = 0; i < 3; i++){
-                var randomNum = Math.floor(Math.random() * 15) + 33;
-                var randomChar = String.fromCharCode(randomNum);
-                randomString = randomString + randomChar.toString();
+            randomString = data[u].Last_Name;
+            if(randomString.charAt(0) != randomString.charAt(0).toUpperCase()){
+                randomString = randomString.replace(randomString.charAt(0) ,randomString.charAt(0).toUpperCase());
+                data[u].Last_Name = randomString;
             }
-            data[u].Password = data[u].Last_Name + randomString;
-        }
-        if(data[u].Employee_id == " " || undefined){
-            data[u].Employee_id = "testid:" + u.toString();
+            if(data[u].Username == " " || undefined){
+                for(i = 0; i < 5; i++){
+                    var randomNum = Math.floor(Math.random() * 26) + 97;
+                    var randomChar = String.fromCharCode(randomNum);
+                    randomString = randomString + randomChar.toString();
+                }
+                data[u].Username = data[u].First_Name + randomString;
+                randomString = "";
+            }
+            if(data[u].Password == " " || undefined){
+                for(i = 0; i < 5; i++){
+                    var randomNum = Math.floor(Math.random() * 10);
+                    //var randomChar = String.fromCharCode(randomNum);
+                    randomString = randomString + randomNum.toString();
+                }
+                for(i = 0; i < 3; i++){
+                    var randomNum = Math.floor(Math.random() * 15) + 33;
+                    var randomChar = String.fromCharCode(randomNum);
+                    randomString = randomString + randomChar.toString();
+                }
+                data[u].Password = data[u].Last_Name + randomString;
+            }
+            if(data[u].Employee_id == " " || undefined){
+                data[u].Employee_id = "testid:" + u.toString();
+            }
         }
     }
 }
@@ -68,79 +82,86 @@ async function login(){
     await driver.findElement(By.name("txtPassword")).sendKeys("admin123");
     await await driver.findElement(By.id("btnLogin")).click();
     for(u = 0; u < data.length; u++){
-        
-        await driver.get('https://opensource-demo.orangehrmlive.com/index.php/pim/addEmployee');
-        await driver.findElement(By.name("chkLogin")).click();
-        await driver.findElement(By.name("firstName")).sendKeys(data[u].First_Name);
-        await driver.findElement(By.name("lastName")).sendKeys(data[u].Last_Name);
-        await driver.findElement(By.name("user_name")).sendKeys(data[u].Username);
-        await driver.findElement(By.name("user_password")).sendKeys(data[u].Password);
-        await driver.findElement(By.name("re_password")).sendKeys(data[u].Password);
-        await driver.findElement(By.name("employeeId")).clear();
-        await driver.findElement(By.name("employeeId")).sendKeys(data[u].Employee_id);
-        await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/form/fieldset/p/input")).click();
-        await new Promise(r => setTimeout(r, 2000));
-        await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/form/fieldset/p/input")).click();
-        
-        if(data[u].Gender == "Male"){
-            await driver.findElement(By.id("personal_optGender_1")).click();
+        if(data[u].Is_Created == "Yes"){
+            continue;
         }
         else{
-            await driver.findElement(By.id("personal_optGender_2")).click();
+            await driver.get('https://opensource-demo.orangehrmlive.com/index.php/pim/addEmployee');
+            await driver.findElement(By.name("chkLogin")).click();
+            await driver.findElement(By.name("firstName")).sendKeys(data[u].First_Name);
+            await driver.findElement(By.name("lastName")).sendKeys(data[u].Last_Name);
+            await driver.findElement(By.name("user_name")).sendKeys(data[u].Username);
+            await driver.findElement(By.name("user_password")).sendKeys(data[u].Password);
+            await driver.findElement(By.name("re_password")).sendKeys(data[u].Password);
+            await driver.findElement(By.name("employeeId")).clear();
+            await driver.findElement(By.name("employeeId")).sendKeys(data[u].Employee_id);
+            await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/form/fieldset/p/input")).click();
+            await new Promise(r => setTimeout(r, 2000));
+            await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/form/fieldset/p/input")).click();
+            
+            if(data[u].Gender == "Male"){
+                await driver.findElement(By.id("personal_optGender_1")).click();
+            }
+            else{
+                await driver.findElement(By.id("personal_optGender_2")).click();
+            }
+            await new Promise(r => setTimeout(r, 250));
+            await driver.findElement(By.id("personal_cmbMarital")).sendKeys(data[u].Marital_Status);
+            await new Promise(r => setTimeout(r, 250));
+            await driver.findElement(By.id("personal_cmbNation")).sendKeys(data[u].Nationality);
+            await new Promise(r => setTimeout(r, 250));
+            //console.log(data[u].Date_of_Birth);
+            await driver.findElement(By.name("personal[DOB]")).clear();
+            await driver.findElement(By.name("personal[DOB]")).sendKeys(data[u].Date_of_Birth);
+            await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/form/fieldset/p/input")).click();
+            await new Promise(r => setTimeout(r, 750));
+            var url = await driver.getCurrentUrl();
+            //console.log(url);
+            var partOfUrl = url.substring(url.indexOf("Number")+7,url.length);
+            //console.log(partOfUrl);
+            await new Promise(r => setTimeout(r, 250));
+            await driver.get('https://opensource-demo.orangehrmlive.com/index.php/pim/contactDetails/empNumber/' + partOfUrl);
+            await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/form/fieldset/p/input")).click();
+
+            await driver.findElement(By.id("contact_street1")).sendKeys(data[u].Address_Street_1);
+            await driver.findElement(By.id("contact_street2")).sendKeys(data[u].Address_Street_2);
+            await driver.findElement(By.id("contact_city")).sendKeys(data[u].City);
+            await driver.findElement(By.id("contact_province")).sendKeys(data[u].State_Province);
+            await driver.findElement(By.id("contact_emp_zipcode")).sendKeys(data[u].Zip_Postal_Code);
+            await driver.findElement(By.id("contact_country")).sendKeys(data[u].Country);
+            await driver.findElement(By.id("contact_emp_hm_telephone")).sendKeys(data[u].Home_Telephone);
+            await driver.findElement(By.id("contact_emp_mobile")).sendKeys(data[u].Mobile);
+            await driver.findElement(By.id("contact_emp_work_telephone")).sendKeys(data[u].Work_Telephone);
+            await driver.findElement(By.id("contact_emp_work_email")).sendKeys(data[u].Work_Email);
+            await driver.findElement(By.id("contact_emp_oth_email")).sendKeys(data[u].Other_Email);
+
+            await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/form/fieldset/p/input")).click();
+            await new Promise(r => setTimeout(r, 750));
+
+            await driver.get('https://opensource-demo.orangehrmlive.com/index.php/pim/viewJobDetails/empNumber/' + partOfUrl);
+            await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[2]/div[2]/form/fieldset/p/input[1]")).click();
+
+            await driver.findElement(By.id("job_job_title")).sendKeys(data[u].Job_Title);
+            await driver.findElement(By.id("job_emp_status")).sendKeys(data[u].Employment_Status);
+            await driver.findElement(By.id("job_eeo_category")).sendKeys(data[u].Job_Category);
+            await driver.findElement(By.id("job_joined_date")).clear();
+            await driver.findElement(By.id("job_joined_date")).sendKeys(data[u].Joined_Date);
+            await driver.findElement(By.id("job_sub_unit")).sendKeys(data[u].Sub_Unit);
+            await driver.findElement(By.id("job_contract_start_date")).clear();
+            await driver.findElement(By.id("job_contract_start_date")).sendKeys(data[u].Start_Date);
+            await driver.findElement(By.id("job_contract_end_date")).clear();
+            await driver.findElement(By.id("job_contract_end_date")).sendKeys(data[u].End_Date);
+            await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[2]/div[2]/form/fieldset/p/input[1]")).click();
+            data[u].Is_Created = "Yes";
+            await new Promise(r => setTimeout(r, 750));
+            
         }
-        await new Promise(r => setTimeout(r, 250));
-        await driver.findElement(By.id("personal_cmbMarital")).sendKeys(data[u].Marital_Status);
-        await new Promise(r => setTimeout(r, 250));
-        await driver.findElement(By.id("personal_cmbNation")).sendKeys(data[u].Nationality);
-        await new Promise(r => setTimeout(r, 250));
-        //console.log(data[u].Date_of_Birth);
-        await driver.findElement(By.name("personal[DOB]")).clear();
-        await driver.findElement(By.name("personal[DOB]")).sendKeys(data[u].Date_of_Birth);
-        await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/form/fieldset/p/input")).click();
-        await new Promise(r => setTimeout(r, 750));
-        var url = await driver.getCurrentUrl();
-        //console.log(url);
-        var partOfUrl = url.substring(url.indexOf("Number")+7,url.length);
-        //console.log(partOfUrl);
-        await new Promise(r => setTimeout(r, 250));
-        await driver.get('https://opensource-demo.orangehrmlive.com/index.php/pim/contactDetails/empNumber/' + partOfUrl);
-        await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/form/fieldset/p/input")).click();
-
-        await driver.findElement(By.id("contact_street1")).sendKeys(data[u].Address_Street_1);
-        await driver.findElement(By.id("contact_street2")).sendKeys(data[u].Address_Street_2);
-        await driver.findElement(By.id("contact_city")).sendKeys(data[u].City);
-        await driver.findElement(By.id("contact_province")).sendKeys(data[u].State_Province);
-        await driver.findElement(By.id("contact_emp_zipcode")).sendKeys(data[u].Zip_Postal_Code);
-        await driver.findElement(By.id("contact_country")).sendKeys(data[u].Country);
-        await driver.findElement(By.id("contact_emp_hm_telephone")).sendKeys(data[u].Home_Telephone);
-        await driver.findElement(By.id("contact_emp_mobile")).sendKeys(data[u].Mobile);
-        await driver.findElement(By.id("contact_emp_work_telephone")).sendKeys(data[u].Work_Telephone);
-        await driver.findElement(By.id("contact_emp_work_email")).sendKeys(data[u].Work_Email);
-        await driver.findElement(By.id("contact_emp_oth_email")).sendKeys(data[u].Other_Email);
-
-        await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/form/fieldset/p/input")).click();
-        await new Promise(r => setTimeout(r, 750));
-
-        await driver.get('https://opensource-demo.orangehrmlive.com/index.php/pim/viewJobDetails/empNumber/' + partOfUrl);
-        await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[2]/div[2]/form/fieldset/p/input[1]")).click();
-
-        await driver.findElement(By.id("job_job_title")).sendKeys(data[u].Job_Title);
-        await driver.findElement(By.id("job_emp_status")).sendKeys(data[u].Employment_Status);
-        await driver.findElement(By.id("job_eeo_category")).sendKeys(data[u].Job_Category);
-        await driver.findElement(By.id("job_joined_date")).clear();
-        await driver.findElement(By.id("job_joined_date")).sendKeys(data[u].Joined_Date);
-        await driver.findElement(By.id("job_sub_unit")).sendKeys(data[u].Sub_Unit);
-        await driver.findElement(By.id("job_contract_start_date")).clear();
-        await driver.findElement(By.id("job_contract_start_date")).sendKeys(data[u].Start_Date);
-        await driver.findElement(By.id("job_contract_end_date")).clear();
-        await driver.findElement(By.id("job_contract_end_date")).sendKeys(data[u].End_Date);
-        await driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[2]/div[2]/form/fieldset/p/input[1]")).click();
-        await new Promise(r => setTimeout(r, 750));
-        
 
     }
     //var today = new Date();
-    
+    wb.Sheets["Sheet1"] = xlsx.utils.json_to_sheet(data);
+
+    xlsx.writeFile(wb,"EmployeeData.xls",{cellDates:true});
 
     for(u = 0; u < data.length; u++){
         if(data[u].Leave_From != " " && data[u].Leave_Assigned == "No"){
